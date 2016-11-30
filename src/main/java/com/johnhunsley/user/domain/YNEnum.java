d630c29aebef98ca50db863e36aa7b623015fc16
johnhunsley <snowball33>
2016-11-30 15:57:27 +0000

    domain and repository

--- src/main/java/com/johnhunsley/user/domain/YNEnum.java
+++ src/main/java/com/johnhunsley/user/domain/YNEnum.java
@@ -2,8 +2,7 @@ package com.johnhunsley.user.domain;
 
 /**
  * @author John Hunsley
- *         (J00074Hunsle)
- *         john.hunsley@avox.info
+ *         jphunsley@gmail.com
  *         Date : 27/09/2016
  *         Time : 15:24
  */
